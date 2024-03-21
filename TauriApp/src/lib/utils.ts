92c16d631986d8b2c55e97fac922211581dc4d41
Beau <beau1wright@gmail.com>
2024-03-21 14:17:40 -0600

    improve console debugging logs

--- TauriApp/src/lib/utils.ts
+++ TauriApp/src/lib/utils.ts
@@ -10,12 +10,15 @@ export function cn(...inputs: ClassValue[]) {
 type ConvertFunction<T> = (json: string) => T;
 
 export async function getObjectFromPythonSidecar<T>(cmdParams: string[], convertFunction: ConvertFunction<T>): Promise<T> {
+  const uuid = crypto.randomUUID()
+  console.log("calling sidecar file with args: ", cmdParams, "time:", new Date, "uuid: ", uuid.toString())
   const command = Command.sidecar("../../PythonInterface/dist/even_photos_resolve", cmdParams);
   const output = await command.execute();
+  console.log("sidecar file returned result for args: ", cmdParams, "time:", new Date, "uuid: ", uuid.toString())
   console.log("cmdParams", cmdParams, "command", command, "stdOut", output.stdout, "stdErr", output.stderr)
   const tempOutput = JSON.parse(output.stdout);
   const json = await readTextFile(tempOutput.path, { dir: BaseDirectory.Temp });
-  console.log(json);
+  //console.log(json);
   await removeFile(tempOutput.path, { dir: BaseDirectory.Temp });
   const convertedData = convertFunction(json);
 
