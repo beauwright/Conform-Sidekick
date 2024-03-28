872c141f7fd1bb919958a9165f52b2b402af398d
Beau <beau1wright@gmail.com>
2024-03-28 13:32:21 -0600

    Remove popovers from toolbar

--- TauriApp/src/components/Toolbar.tsx
+++ TauriApp/src/components/Toolbar.tsx
@@ -1,6 +1,4 @@
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { InfoCircledIcon } from "@radix-ui/react-icons";
-import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 function Toolbar({
   defaultValue,
   onViewChange,
@@ -12,7 +10,7 @@ function Toolbar({
     <>
       <Tabs
         defaultValue={defaultValue}
-        className="w-[400] px-10 pt-10 pb-0 lg:px-20 lg:pt-20"
+        className="w-[400] p-10 lg:px-20 lg:pt-20"
       >
         <TabsList className="grid w-full grid-cols-3 h-12">
           <TabsTrigger
@@ -38,50 +36,6 @@ function Toolbar({
           </TabsTrigger>
         </TabsList>
       </Tabs>
-      <div className="grid w-full grid-cols-3 pt-2 pb-10 px-10">
-        <Popover>
-          <PopoverTrigger>
-            <InfoCircledIcon className="text-slate-400" />
-          </PopoverTrigger>
-          <PopoverContent align="start">
-            <h3 className="text-sm break-words">
-              Odd resolution media can cause export errors when rendering at
-              source resolution for codecs that do not support odd resolutions.
-              <br />
-              <br />
-              This tool allows you to quickly identify any media with odd
-              resolutions and can fix the issue automatically if the media is a
-              photo.
-            </h3>
-          </PopoverContent>
-        </Popover>
-        <Popover>
-          <PopoverTrigger>
-            <InfoCircledIcon className="text-slate-400" />
-          </PopoverTrigger>
-          <PopoverContent align="start">
-            <h3 className="text-sm break-words">
-              Interlaced footage often needs special care from the colorist.
-              <br />
-              <br />
-              This tool allows you to identify interlaced footage quickly.
-            </h3>
-          </PopoverContent>
-        </Popover>
-        <Popover>
-          <PopoverTrigger>
-            <InfoCircledIcon className="text-slate-400" />
-          </PopoverTrigger>
-          <PopoverContent align="start">
-            <h3 className="text-sm break-words">
-              Compound clips can require special care from the colorist.
-              <br />
-              <br />
-              This tool allows you to identify compound clips quickly.
-            </h3>
-          </PopoverContent>
-        </Popover>
-      </div>
     </>
   );
 }
